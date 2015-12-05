cd7e039c7b05cc3914f6343c9e7334df05cd9d99
Burste <sppqre@yahoo.com.tw>
2015-12-05 06:04:05 +0000

    promise note

---
+++ fooDemo.js
@@ -0,0 +1,7 @@
+foo();//使用function可以執行 用 var foo=function(){}無法
+
+function foo(){
+    console.log("Hello world");
+    
+}
+
--- promiseParallel_demo.js
+++ promiseParallel_demo.js
@@ -1,4 +1,4 @@
-const promise = require('bluebird');//assign function
+const promise = require('bluebird');
 
 const function1 = function(callback){
     console.log('function1 done');
@@ -12,16 +12,27 @@ const function2 = function(callback) {
     }, 3000);
 };
 
+const function3 = function(callback) {
+    setTimeout(function(){
+        console.log('function3 done');
+        return callback();
+    }, 1000);
+};
+
 const pfunction1 = promise.promisify(function1);
 const pfunction2 = promise.promisify(function2);
-
-promise.resolve(pfunction2())//then當成呼叫另一個callback function
+const pfunction3 = promise.promisify(function3);
+//promise可以幫你區分
+promise.all([
+    pfunction3(),
+    pfunction2()
+])//3,2都執行完才會執行1
 .then(function() {
     return pfunction1();
 })
 .then(function() {
     console.log('done!!');
 })
-.error(function(err){//接收錯誤值
+.error(function(error) {
     console.log('error!!');
 });
