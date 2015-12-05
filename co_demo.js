1269c39dbbe72adef7a212a70dd0e480e7fec2cf
Burste <sppqre@yahoo.com.tw>
2015-12-05 06:10:54 +0000

    initial commit

--- co_demo.js
+++ co_demo.js
@@ -1,4 +1,5 @@
 const promise = require('bluebird');
+const co = require('co');
 
 const function1 = function(callback){
     console.log('function1 done');
@@ -12,27 +13,15 @@ const function2 = function(callback) {
     }, 3000);
 };
 
-const function3 = function(callback) {
-    setTimeout(function(){
-        console.log('function3 done');
-        return callback();
-    }, 1000);
-};
-
 const pfunction1 = promise.promisify(function1);
 const pfunction2 = promise.promisify(function2);
-const pfunction3 = promise.promisify(function3);
-//promise可以幫你區分事件
-promise.all([
-    pfunction3(),
-    pfunction2()
-])//3,2都執行完才會執行1
-.then(function() {
-    return pfunction1();
+co(function*() {
+    yield pfunction2();
+    yield pfunction1();
 })
 .then(function() {
     console.log('done!!');
 })
-.error(function(error) {
+.catch(function(err) {
     console.log('error!!');
 });
