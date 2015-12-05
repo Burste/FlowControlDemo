ab3e953ddb28290cbb00d5acfeb169cb1b8554e0
Burste <sppqre@yahoo.com.tw>
2015-12-05 07:03:02 +0000

    initial commit

---
+++ Oauth/hashPwd.js
@@ -0,0 +1,12 @@
+const sha1 = require('sha1');
+
+// 你傳入的密碼
+const password = 'simonPWD';
+
+// 加入一段固定字串
+const hashString = '.$AWed(!!';
+
+// 加密
+var hashPwd = sha1(hashString + password);
+
+console.log(hashPwd);
